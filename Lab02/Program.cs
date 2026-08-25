2fb2b377bedbc43cd49852a2653a903d53778cd5
rachtachin <rachta.chin@bumail.net>
2026-08-25 17:09:26 +0700

    Lab02 hpboss

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID :1690704828
+ * Name       :Lab02
+ * Section    :129D
+ * No.        :0
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+            // LAb02 pA
+            Console.WriteLine("===== BOSS STATUS: INITIAL ===== ");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"level: {level}");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Attack power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+            Console.WriteLine();
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp percent: {hpPercent}%");
+            Console.WriteLine();
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMGE =====");
+            Console.WriteLine($"Hp:");
+
+
+
+
+
+
         }
     }
 }
